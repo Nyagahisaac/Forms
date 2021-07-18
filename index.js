148149c52d024993e1ef6fa2407282f38261ea85
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-07-18 15:34:55 +0300

    Ankan names added in javascript

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 function submitName(){
     var dateTime = document.getElementById("date").value;
-    console.log(dateTime);
-   
+    // console.log(dateTime);
+    var maleAnkan = [
+        'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'
+    ];
+    var femaleAnkan = [
+        'Akosua', 'Adwoa', 'Abeena', 'Akua', 'Yaa', 'Afua', 'Ama'
+    ];
+    
+  
+    
 }
