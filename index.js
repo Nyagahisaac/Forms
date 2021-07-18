f2a50d6f2dda07c3b4871deb229a0d86c1a07b8a
isaac.ireri <isaac.ireri@student.moringaschool.com>
2021-07-18 21:08:54 +0300

    add copyright

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 function submitName(){
+    alert('FORM HAS BEEN SUBMITED')
     var dateTime = document.getElementById("date").value;
     // console.log(dateTime);
+    // var D = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
     var maleAnkan = [
         'Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame'
     ];
@@ -21,6 +23,8 @@ function submitName(){
      var namesF = femaleAnkan[femaleindex];   
 
     }
-    console.log(namesM)
+    var newName  = document.getElementById("").innerHTML = date.getDay();
+    // var  = document.getElementById("male").innerHTML = date.getDay();
+    //  console.log(namesF)
     
 }
